32a1fd19c9801b0d91b8edafff2c72b729e85a87
Jason Argo <jason@argolytics.io>
2023-03-13 18:36:11 -0400

    Added DriverPathSettings in appsettings.json

--- DataLibrary/Settings/DriverPathSettings.cs
+++ DataLibrary/Settings/DriverPathSettings.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DataLibrary.Settings;
 
-namespace DataLibrary.Settings
+public class DriverPathSettings
 {
-    public class DriverPathSettings
-    {
-        public string? FirefoxProfilePath { get; set; }
-        public string? GeckoDriverPath { get; set; }
-    }
+    public string? FirefoxProfilePath { get; set; }
+    public string? GeckoDriverPath { get; set; }
 }
